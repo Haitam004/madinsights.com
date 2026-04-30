e3e95485b1c4917ab71480f781c54f8cc04bf180
Haitam004 <haitamandaloussi231@gmail.com>
2026-04-30 17:48:50 +0100

    ajustement mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,8 @@ const inter = Inter({
 });
 
 export const metadata = {
-  themeColor: "#020617"
+  themeColor: "#020617",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1"
 };
 
 export default function RootLayout({
